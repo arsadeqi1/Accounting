46e1aea8db3f021d92201069acce6ff7bafd4912
Alireza Sadeghi <arsadeqi1@mail.com>
2022-08-17 16:08:41 +0430

    Interface ICustomerRepository written

---
+++ Accounting.DataLayer/Repostories/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repostories
+{
+    public interface ICustomerRepository
+    {
+        List<Customers> GetAllCustomers();
+        List<Customers> GetCustomerById(int customer);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(Customers customer);
+        bool DeleteCustomer(int customerId);
+        void Save();
+    }
+}
